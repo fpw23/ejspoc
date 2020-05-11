000fbd54cf5515c189a27ddda3c6e48439948da7
Frank Wisniewski <me@frankwisniewski.net>
2020-05-11 07:57:49 -0500

    Updates

---
+++ precache-manifest.ed7777bd983079286a1d89293b40f136.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a41324b122cd77f8d17ce73d620a941",
+    "url": "/ejspoc/index.html"
+  },
+  {
+    "revision": "45a1902f1c502a4dd36a",
+    "url": "/ejspoc/static/css/main.9845e80f.chunk.css"
+  },
+  {
+    "revision": "3bb5bb32c7dcffcb5b92",
+    "url": "/ejspoc/static/js/2.56022111.chunk.js"
+  },
+  {
+    "revision": "fe5ed21bab0ef872fb1eb4dae7cf95e7",
+    "url": "/ejspoc/static/js/2.56022111.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "45a1902f1c502a4dd36a",
+    "url": "/ejspoc/static/js/main.8353919d.chunk.js"
+  },
+  {
+    "revision": "84f8aa3792d9c399c8c4",
+    "url": "/ejspoc/static/js/runtime-main.d593ba2a.js"
+  }
+]);
